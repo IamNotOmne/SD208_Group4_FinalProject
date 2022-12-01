7d37bba81190bc00c855abf85726a9cb3b8b1af5
Mark Steven Geraga <markstevengeraga@gmail.com>
2022-12-01 17:16:11 +0800

    FINAL REGISTRY FORM
    This is the final registry form with cookies and session and database. This is also ready to be merged.

--- php/ediarycookies.php
+++ php/ediarycookies.php
@@ -1,9 +1,14 @@
 <?php
+    ob_start();
+    session_start();
+    require_once('connect.inc');
+
     if(isset($_POST['login']))
         { 
         $email = $_POST['email'];
         $password = $_POST['password'];
         $remember = isset($_POST['remember']);
+
         if($remember) {
             setcookie($email, $password, time() + (3600 * 30), "/"); //one hour
             if(count($_COOKIE) > 0) {
@@ -13,17 +18,22 @@
               }
         }
 
-        header("Location: https://geragacv.github.io/");
-        exit();
-        
-        require "valuesource.php";
-        if($email == $signedemail && $password == $signedpassword) {
-
-        }else if($email != $signedemail && $password == $signedpassword) {
-          echo '<script>alert("We could not find an account with that email address")</script>';
-        }else if($email == $signedemail && $password != $signedpassword) {
-          echo '<script>alert("Incorrect Password")</script>';
-        }else {
-          echo '<script>alert("No existing account")</script>';
+        $passwordMD5 = md5($password);
+        $user_query = "select password from register where email = '$email'";
+        $result_query = mysqli_query($conn, $user_query) or die("Cannot connect to Table");
+        if(mysqli_num_rows($result_query) == 1){
+            $dbpassword = mysqli_fetch_row($result_query)[0];
+            if($dbpassword == $passwordMD5){
+                $_SESSION['email'] = $email;
+                $_SESSION['timeout'] = time();
+                $_SESSION['logged_in'] = true;
+                header("Location: https://youtu.be/nr2CsEtXxmI");
+                
+            }else{
+                echo '<script>alert("Login failed. Wrong username or password")</script>';
+            }
+        } else{
+          echo '<script>alert("Login failed. Wrong username or password")</script>';
         }
-}?>
+}
+?>
--- register.php
+++ register.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <?php include './php/valuesource.php';?>
 <html lang="en">
@@ -7,14 +8,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>E-Diary</title>
-    <link rel="icon" href="./img/logo.png" />
+    <link rel="icon" href="Logo.png" />
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css" />
-    <link rel="stylesheet" href="./css/register.css" />
+    <link rel="stylesheet" href="css/register.css" />
 </head>
 
-<body>
+<body> 
+    
+<!-- connect to database -->
     <div class="logo">
-        <img src="./img/logo.png" alt="Logo" />
+        <img src="img/logo.png" alt="Logo" />
     </div>
     <div class="container">
         <div class="forms">
@@ -26,33 +29,59 @@
                 <form action="register.php" method="post">
                   
                     <div class="input-field">
-                        <input id="username" type="text" placeholder="Username" name="username" required/>
+                        <input name="username" type="text" placeholder="Username" required />
                         <i class="uil uil-user icon"></i>
                     </div>
-
                     <div class="input-field">
-                        <input id="username" type="text" placeholder="Name@gmail.com" name="email" required/>
+                        <input name="email" type="text" placeholder="Name@gmail.com" required />
                         <i class="uil uil-envelope icon"></i>
                     </div>
-
                     <div class="input-field">
-                        <input id="password" type="Password" class="Password" placeholder="Password" name="password" required/>
+                        <input name="password" type="password" class="password" placeholder="Password" required />
                         <i class="uil uil-lock icon"></i>
                         <i class="uil uil-eye-slash showHidePw"></i>
                     </div>
-                   
 
-                    <div class="input-field button">
-                        <input class="signup-sub" type="submit" value="SIGN UP" name="signup" id="submit"/>
-                    </div>
+                    <button type="submit" class="signup" name="submit" style=" border: none; color: #fff; font-size: 17px;
+                    width: 100%; height:45px; font-weight: 500; letter-spacing: 1px; border-radius: 15px; background-color: black;
+                    cursor: pointer; margin-top: 35px; transition: all 0.3s ease;">SIGN UP</button>
+
                 </form>
-                
-
-                <div class="signup-login">
-                    <a href="login.php" class="text">Already have an Account?</a><br />
+                <div class="login-signup">
+                    <a href="login.php" class="signup">Create New Account</a><br />
                 </div>
             </div>
         </div>
     </div>
-</body>
 
+    <script>
+
+        const container = document.querySelector(".container"),
+        pwShowHide = document.querySelectorAll(".showHidePw"),
+        pwFields = document.querySelectorAll(".password");
+
+    //this is to show/hide password
+    
+    pwShowHide.forEach((eyeIcon) => {
+        eyeIcon.addEventListener("click", () => {
+            pwFields.forEach((pwField) => {
+                if (pwField.type === "password") {
+                    pwField.type = "text";
+
+                    pwShowHide.forEach((icon) => {
+                        icon.classList.replace("uil-eye-slash", "uil-eye");
+                    });
+                } else {
+                    pwField.type = "password";
+                    
+                    pwShowHide.forEach((icon) => {
+                        icon.classList.replace("uil-eye", "uil-eye-slash");
+                    });
+                }
+            });
+        });
+    });
+</script>
+
+</body>
+</html>
