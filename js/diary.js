d1599d513943f26bf10a326e4786c5d07497d93a
Ara <21103865@usc.edu.ph>
2022-12-09 11:51:12 +0800

    edited the js file and the switch button

---
+++ js/diary.js
@@ -0,0 +1,14 @@
+
+function menuToggle() {
+    const toggleMenu = document.querySelector('.userMenu');
+    toggleMenu.classList.toggle('active')
+}
+
+function iconToggle(){
+        document.body.classList.toggle("dark-theme");
+        // document.addEventListener("click",(event)=>{
+        //     event.preventDefault();
+            
+        // });  
+          
+}
