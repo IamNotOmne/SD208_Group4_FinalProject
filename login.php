26102918cb0c3729f07ccb09ee4b67629e4a4fbc
Mark Steven Geraga <markstevengeraga@gmail.com>
2022-12-09 12:47:03 +0800

    Final LOGIN/LOGOUT
    Trying to merge.....

--- login.php
+++ login.php
@@ -1,20 +1,53 @@
-<!DOCTYPE html>
-<?php include './php/ediarycookies.php';?>
-<html lang="en">
+<?php 
+ob_start();
+session_start();
+require_once('includes/connect.inc');
+?>
+<?php 
+   if(isset($_POST['login'])) { 
+   $email = $_POST['email'];
+   $password = md5($_POST['password']);
+   $remember = isset($_POST['remember']);
 
+   if($remember) {
+       setcookie($email, $password, time() + (3600 * 30), "/"); //one hour
+       if(count($_COOKIE) > 0) {
+           echo '<script>alert("Cookies are active.")</script>';
+         } else {
+           echo '<script>alert("Cookies Deactivated.")</script>';
+         }
+   }
+   
+   $user_query ="SELECT * FROM register where email ='$email' AND password ='$password'";
+   $query= mysqli_query($conn, $user_query);
+   $count = mysqli_num_rows($query);
+   if($count > 0)
+   {
+       while ($row = mysqli_fetch_assoc($query)) {
+          $_SESSION['logged_in']=$row['user_ID'];
+          echo "<script type='text/javascript'> document.location = 'diary.php'; </script>";
+       }
+   } 
+   else{
+     echo "<script>alert('Invalid Login!');</script>";
+   }
+}
+?>
+
+<html>
 <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>E-Diary</title>
-    <link rel="icon" href="Logo.png" />
+    <link rel="icon" href="img/logo.png" />
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css" />
     <link rel="stylesheet" href="./css/login.css" />
 </head>
 
 <body>
     <div class="logo">
-        <img src="./img/Logo.png" alt="Logo" />
+        <img src="img/logo.png" alt="Logo" />
     </div>
     <div class="container">
         <div class="forms">
