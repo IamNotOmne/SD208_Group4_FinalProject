b99993755c765312c509e0fb8fea2f0de26b4e13
Ara <21103865@usc.edu.ph>
2022-12-12 19:46:08 +0800

    edited the switch function in js

--- js/diary.js
+++ js/diary.js
@@ -4,11 +4,30 @@ function menuToggle() {
     toggleMenu.classList.toggle('active')
 }
 
+
 function iconToggle(){
-        document.body.classList.toggle("dark-theme");
-        // document.addEventListener("click",(event)=>{
-        //     event.preventDefault();
-            
-        // });  
-          
+    var SetTheme = document.body;
+    SetTheme.classList.toggle("dark-theme");
+
+    var theme;
+
+    if(SetTheme.classList.contains("dark-theme")){
+         console.log("Dark Theme");
+         theme = "DARK";
+    }else{
+        console.log("Light Theme");
+        theme = "LIGHT";
+    }
+    localStorage.setItem("PageTheme", JSON.stringify(theme));
+// document.addEventListener("click",(event)=>{
+//         event.preventDefault();
+}
+
+let GetTheme = JSON.parse(localStorage.getItem("PageTheme"));
+console.log(GetTheme);
+
+if(GetTheme === "DARK"){
+    document.body.classList = "dark-theme";
+
+
 }
